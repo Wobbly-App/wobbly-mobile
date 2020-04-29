37cb389a6e3a0cc62b10b3b716b9f32d228877dd
an-k45 <an_k27@protonmail.com>
2020-04-29 13:43:23 -0400

    Refactor LoginScreen as a class component

--- src/components/screens/LoginScreen.tsx
+++ src/components/screens/LoginScreen.tsx
@@ -1,8 +1,29 @@
+import { Formik, FormikProps } from 'formik';
 import React from 'react';
-import { Button, SafeAreaView, Text } from 'react-native';
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+} from 'react-native';
 import { ConnectedProps, connect } from 'react-redux';
 
 import { login } from '../../redux/modules/auth';
+import WobblyText from '../atoms/WobblyText';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcomeHeading: {
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+});
 
 const mapDispatch = {
   login,
@@ -10,17 +31,23 @@ const mapDispatch = {
 const connector = connect(undefined, mapDispatch);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
-const LoginScreen: React.FC<PropsFromRedux> = ({ login }) => {
-  const loginFlow = () => {
-    login('dev@xmpp.wobbly.app', 'Cee9ech4Ia6wupho');
-  };
+class LoginScreen extends React.PureComponent<PropsFromRedux> {
+  loginFlow() {
+    this.props.login('dev@xmpp.wobbly.app', 'Cee9ech4Ia6wupho');
+  }
 
-  return (
-    <SafeAreaView>
-      <Text>Please login!</Text>
-      <Button onPress={loginFlow} title="Login" />
-    </SafeAreaView>
-  );
-};
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <WobblyText title2={true} style={styles.welcomeHeading}>
+          Login
+        </WobblyText>
+        <TextInput placeholder="email" />
+        <TextInput placeholder="password" />
+        <Button onPress={this.loginFlow.bind(this)} title="Login" />
+      </SafeAreaView>
+    );
+  }
+}
 
 export default connector(LoginScreen);
