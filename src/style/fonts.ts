901ffd1190a0c00da9bd525651dc5ad3c18b6ee8
an-k45 <an_k27@protonmail.com>
2020-05-01 16:04:56 -0400

    Add fonts from archived repository

---
+++ src/style/fonts.ts
@@ -0,0 +1,7 @@
+export const fonts = {
+  'open-sans-regular': require('../assets/fonts/OpenSans-Regular.ttf'),
+  'open-sans-bold': require('../assets/fonts/OpenSans-Bold.ttf'),
+  'montserrat-regular': require('../assets/fonts/Montserrat-Regular.ttf'),
+  'montserrat-bold': require('../assets/fonts/Montserrat-Bold.ttf'),
+  'montserrat-black': require('../assets/fonts/Montserrat-Black.ttf'),
+};
